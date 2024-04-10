d33db9565387aedc450e2472a866511b762a7989
Amit Raikwar <managerarbusiness@gmail.com>
2024-04-11 03:15:31 +0530

    Fix IconApi import

--- src/assets/icons/social_icon/IconApi.tsx
+++ src/assets/icons/social_icon/IconApi.tsx
@@ -1,11 +1,11 @@
 import { SocialType } from "../../../service/supabase/supastore/user_profile/UserCollection";
 import FacebookIcon from "./FacebookIcon";
-import { GithubIcon } from "./GithubIcon";
-import { InstagramIcon } from "./InstagramIcon";
-import { LinkedInIcon } from "./LinkedInIcon";
-import { WebsiteIcon } from "./WebsiteIcon";
-import { XIcon } from "./XIcon";
-import { YoutubeIcon } from "./YoutubeIcon";
+import GithubIcon from "./GithubIcon";
+import InstagramIcon from "./InstagramIcon";
+import LinkedInIcon from "./LinkedInIcon";
+import WebsiteIcon from "./WebsiteIcon";
+import XIcon from "./XIcon";
+import YoutubeIcon from "./YoutubeIcon";
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /**
