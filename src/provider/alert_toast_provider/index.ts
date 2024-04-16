6e27348d89a0365d1926fbde3806f48d672d9a67
Amit Raikwar <managerarbusiness@gmail.com>
2024-04-16 23:56:36 +0530

    [AlertProvider]: Added index.ts structure

---
+++ src/provider/alert_toast_provider/index.ts
@@ -0,0 +1,11 @@
+export { default as AlertToastProvider } from "./AlertToastProvider";
+export type {
+  IToastAlertProviderProps,
+  ToastAlertData,
+} from "./AlertToastProvider";
+export {
+  getSuccessAlertData,
+  getErrorAlertData,
+  getInfoAlertData,
+  getWarningAlertData,
+} from "./AlertToastProvider";
---
+++ src/provider/index.ts
@@ -0,0 +1,1 @@
+export * from "./alert_toast_provider";
