3d07b109b73e74bedc96f1150cd084f25a11151b
Amit Raikwar <managerarbusiness@gmail.com>
2024-04-18 03:40:58 +0530

    Refactor and cleanup PageLoading component and its index file

--- src/components/page_loading/PageLoading.tsx
+++ src/components/page_loading/PageLoading.tsx
@@ -1,33 +1,17 @@
-/**
- * Author: Amit raikwar
- * Last updated: 04 Feb, 2024
- */
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import { PageLoadingComponentProps } from "./types";
 
-/**
- * Page loading component props.
- */
-interface IPageLoadingComponentProps {}
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /**
  * Page loading component.
  *
  * @param props  The page loading component props.
  * @returns The page loading component.
  */
-function PageLoadingComponent(props: IPageLoadingComponentProps) {
+const PageLoadingComponent = (props: PageLoadingComponentProps) => {
   return (
     <div className=" h-full w-full justify-center items-center flex ">
       <span className="loading loading-spinner loading-lg text-primary"></span>
     </div>
   );
-}
+};
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Export the PageLoading component.
 export default PageLoadingComponent;
-export type { IPageLoadingComponentProps };
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// End of file.
